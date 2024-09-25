a1a19dc606e9e1abae55e54cfed702d168340435
Alchemy <angeloleandro.mkt@gmail.com>
2024-09-25 11:20:03 -0300

    Finalizando a tradução do item de ferramentas

--- components/sidebar/items/tools/tool-item.tsx
+++ components/sidebar/items/tools/tool-item.tsx
@@ -1,48 +1,43 @@
-import { SidebarCreateItem } from "@/components/sidebar/items/all/sidebar-create-item"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { TextareaAutosize } from "@/components/ui/textarea-autosize"
-import { ChatbotUIContext } from "@/context/context"
 import { TOOL_DESCRIPTION_MAX, TOOL_NAME_MAX } from "@/db/limits"
 import { validateOpenAPI } from "@/lib/openapi-conversion"
-import { TablesInsert } from "@/supabase/types"
-import { FC, useContext, useState } from "react"
+import { Tables } from "@/supabase/types"
+import { IconBolt } from "@tabler/icons-react"
+import { FC, useState } from "react"
 import { useTranslation } from "react-i18next"
+import { SidebarItem } from "../all/sidebar-display-item"
 
-interface CreateToolProps {
-  isOpen: boolean
-  onOpenChange: (isOpen: boolean) => void
+interface ToolItemProps {
+  tool: Tables<"tools">
 }
 
-export const CreateTool: FC<CreateToolProps> = ({ isOpen, onOpenChange }) => {
+export const ToolItem: FC<ToolItemProps> = ({ tool }) => {
   const { t } = useTranslation()
-  const { profile, selectedWorkspace } = useContext(ChatbotUIContext)
-
-  const [name, setName] = useState("")
+  const [name, setName] = useState(tool.name)
   const [isTyping, setIsTyping] = useState(false)
-  const [description, setDescription] = useState("")
-  const [url, setUrl] = useState("")
-  const [customHeaders, setCustomHeaders] = useState("")
-  const [schema, setSchema] = useState("")
+  const [description, setDescription] = useState(tool.description)
+  const [url, setUrl] = useState(tool.url)
+  const [customHeaders, setCustomHeaders] = useState(
+    tool.custom_headers as string
+  )
+  const [schema, setSchema] = useState(tool.schema as string)
   const [schemaError, setSchemaError] = useState("")
 
-  if (!profile || !selectedWorkspace) return null
-
   return (
-    <SidebarCreateItem
-      contentType="tools"
-      createState={
-        {
-          user_id: profile.user_id,
-          name,
-          description,
-          url,
-          custom_headers: customHeaders,
-          schema
-        } as TablesInsert<"tools">
-      }
-      isOpen={isOpen}
+    <SidebarItem
+      item={tool}
       isTyping={isTyping}
+      contentType="tools"
+      icon={<IconBolt size={30} />}
+      updateState={{
+        name,
+        description,
+        url,
+        custom_headers: customHeaders,
+        schema
+      }}
       renderInputs={() => (
         <>
           <div className="space-y-1">
@@ -67,6 +62,36 @@ export const CreateTool: FC<CreateToolProps> = ({ isOpen, onOpenChange }) => {
             />
           </div>
 
+          {/* <div className="space-y-1">
+            <Label>{t("url")}</Label>
+
+            <Input
+              placeholder={t("toolUrlPlaceholder")}
+              value={url}
+              onChange={e => setUrl(e.target.value)}
+            />
+          </div> */}
+
+          {/* <div className="space-y-3 pt-4 pb-3">
+            <div className="space-x-2 flex items-center">
+              <Checkbox />
+
+              <Label>{t("webBrowsing")}</Label>
+            </div>
+
+            <div className="space-x-2 flex items-center">
+              <Checkbox />
+
+              <Label>{t("imageGeneration")}</Label>
+            </div>
+
+            <div className="space-x-2 flex items-center">
+              <Checkbox />
+
+              <Label>{t("codeInterpreter")}</Label>
+            </div>
+          </div> */}
+
           <div className="space-y-1">
             <Label>{t("customHeaders")}</Label>
 
@@ -82,7 +107,42 @@ export const CreateTool: FC<CreateToolProps> = ({ isOpen, onOpenChange }) => {
             <Label>{t("schema")}</Label>
 
             <TextareaAutosize
-              placeholder={t("schemaPlaceholder")}
+              placeholder={`{
+                "openapi": "3.1.0",
+                "info": {
+                  "title": "Get weather data",
+                  "description": "Retrieves current weather data for a location.",
+                  "version": "v1.0.0"
+                },
+                "servers": [
+                  {
+                    "url": "https://weather.example.com"
+                  }
+                ],
+                "paths": {
+                  "/location": {
+                    "get": {
+                      "description": "Get temperature for a specific location",
+                      "operationId": "GetCurrentWeather",
+                      "parameters": [
+                        {
+                          "name": "location",
+                          "in": "query",
+                          "description": "The city and state to retrieve the weather for",
+                          "required": true,
+                          "schema": {
+                            "type": "string"
+                          }
+                        }
+                      ],
+                      "deprecated": false
+                    }
+                  }
+                },
+                "components": {
+                  "schemas": {}
+                }
+              }`}
               value={schema}
               onValueChange={value => {
                 setSchema(value)
@@ -90,10 +150,10 @@ export const CreateTool: FC<CreateToolProps> = ({ isOpen, onOpenChange }) => {
                 try {
                   const parsedSchema = JSON.parse(value)
                   validateOpenAPI(parsedSchema)
-                    .then(() => setSchemaError(""))
-                    .catch(error => setSchemaError(error.message))
+                    .then(() => setSchemaError("")) // Clear error if validation is successful
+                    .catch(error => setSchemaError(error.message)) // Set specific validation error message
                 } catch (error) {
-                  setSchemaError(t("invalidJSONFormat"))
+                  setSchemaError(t("invalidJSONFormat")) // Set error for invalid JSON format
                 }
               }}
               minRows={15}
@@ -103,7 +163,6 @@ export const CreateTool: FC<CreateToolProps> = ({ isOpen, onOpenChange }) => {
           </div>
         </>
       )}
-      onOpenChange={onOpenChange}
     />
   )
 }
