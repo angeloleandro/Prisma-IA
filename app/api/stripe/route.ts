5f19b0a4bbedecbfb06d5d3e4331012416a67fac
Alchemy <angeloleandro.mkt@gmail.com>
2024-09-30 20:10:28 -0300

    feat: Add upgradeToProStatus function to update user profile to Pro status

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -7,7 +7,8 @@ export async function POST(req: Request) {
     console.log("Creating Stripe session for user:", userId)
 
     // Usar o URL base da aplicação
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
+    const baseUrl =
+      process.env.NEXT_PUBLIC_BASE_URL || "https://prisma-ia.vercel.app"
 
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
